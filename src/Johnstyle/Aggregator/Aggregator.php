612e071fa51b6b9b8eb7adf7d2345c7a48aa4de9
Jonathan SAHM <contact@johnstyle.fr>
2015-01-25 00:36:08 +0100

    Update display()

--- src/Johnstyle/Aggregator/Aggregator.php
+++ src/Johnstyle/Aggregator/Aggregator.php
@@ -21,15 +21,36 @@ namespace Johnstyle\Aggregator;
 class Aggregator
 {
     const LANGUAGE = 'en-US';
+    const CHARSET = 'UTF-8';
+    const CACHE_TIME = 3600;
 
     /**
-     * @param string $title
-     * @param string $description
-     * @param string $items
+     * @param string   $title
+     * @param string   $description
+     * @param \closure $callback
      */
-    public static function display($title, $description, $items)
+    public static function display($title, $description, \closure $callback)
     {
-        include 'View/rss2.php';
+        $file = sys_get_temp_dir() . '/' . md5($title);
+
+        if(!file_exists($file)
+            || filemtime($file) > time() + static::CACHE_TIME) {
+
+            $items = call_user_func_array($callback, array());
+
+            ob_start(function($content) use($file) {
+                file_put_contents($file, $content);
+                return null;
+            });
+
+            include 'View/rss2.php';
+
+            ob_end_flush();
+        }
+
+        header('Content-Type: application/rss+xml; charset=' . static::CHARSET);
+        header('Content-Size: ' . filesize($file));
+        readfile($file);
     }
 
     /**
